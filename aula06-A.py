b9dce448eacd80a0adfcd08b8da6cfc74ad1b7dd
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2022-11-17 00:16:25 -0500

    Lesson-06-A

---
+++ aula06-A.py
@@ -0,0 +1,12 @@
+'''
+# n1 = int(input('Digite um valor: '))
+# n2 = int(input('Digite outro valor: '))
+# s = n1 + n2
+# print('A soma entre', n1, 'e', n2, 'vale', s)
+# print('A soma entre {} e {} vale {}'.format(n1, n2, s)) '''
+
+n1 = int(input('Enter a value: '))
+n2 = int(input('Enter another value: '))
+s = n1 + n2
+print('The sum between', n1, 'and', n2, 'is', s)
+print('The sum between {} and {} is {}'.format(n1, n2, s))
