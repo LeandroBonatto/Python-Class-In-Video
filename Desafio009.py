959ccc6542d94ea002cea1ded95c7603a6ace889
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2022-11-19 00:20:37 -0500

    Challenge-9

---
+++ Desafio009.py
@@ -0,0 +1,8 @@
+'''
+num1 = int(input('Digite um numero: '))
+tabuada = num1*1, num1*2, num1*3, num1*4, num1*5, num1*6, num1*7, num1*8, num1*9, num1*10
+print('Digite a tabuada do numero mencionado {}'.format(tabuada))'''
+
+num1 = int(input('Enter a number: '))
+mult = num1*1, num1*2, num1*3, num1*4, num1*5, num1*6, num1*7, num1*8, num1*9, num1*10
+print('Enter the multiplication table of the mentioned number {}'.format(mult))
