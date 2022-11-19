6f3246047f9e006de25dac5770a90b0bf6542017
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2022-11-19 00:19:16 -0500

    Challenge-7-Ex

---
+++ desafio007ex.py
@@ -0,0 +1,10 @@
+'''
+n1 = float(input('Primeira nota: '))
+n2 = float(input('Segunda nota: '))
+m = (n1 + n2) / 2
+print('A media entre {:.1f} e {:.1f} eh igual a {:.1f}'.format(n1, n2, m))'''
+
+n1 = float(input('First score: '))
+n2 = float(input('Second score: '))
+ave = (n1 + n2) / 2
+print('The average between {:.1f} and {:.1f} is {:.1f}'.format(n1, n2, ave))
