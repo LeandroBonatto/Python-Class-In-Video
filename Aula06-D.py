bf08f9bdded2875d44086ca5f2fb3c4e230ba3f1
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2022-11-18 22:45:40 -0500

    06-D

---
+++ Aula06-D.py
@@ -0,0 +1,10 @@
+'''
+# n1 = input('Digite o primeiro valor: ')
+# n2 = input('Digete o segundo valor: ')
+# s = float(n1) + float(n2)
+# print('A soma vale {:.2f}'.format(s)) '''
+
+n1 = input('Enter first value: ')
+n2 = input('Enter second value: ')
+s = float(n1) + float(n2)
+print('The sum is {:.2f}'.format(s))
