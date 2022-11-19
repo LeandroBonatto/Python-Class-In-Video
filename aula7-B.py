cdfe5f15ef46d1ecef2af6f30e538ea18a0435e2
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2022-11-19 00:16:40 -0500

    07-B

---
+++ aula7-B.py
@@ -0,0 +1,20 @@
+'''
+n1 = int(input('Um valor: '))
+n2 = int(input('Outro valor: '))
+s = n1 + n2
+m = n1 * n2
+d = n1 / n2
+di = n1 // n2
+e = n1 ** n2
+print('A soma vale {}, o produto vale {} e a divisao vale {:.3}'.format(s, m, d))
+print('Divisao inteira {} e potencia {}'.format(di, e))'''
+
+n1 = int(input('Enter first number: '))
+n2 = int(input('Enter second number: '))
+s = n1 + n2
+m = n1 * n2
+d = n1 / n2
+di = n1 // n2
+e = n1 ** n2
+print('The sum is {}, product is {}, division is {:.2f}'.format(s, m, d))
+print('Integer division {} and power {}'.format(di, e))
