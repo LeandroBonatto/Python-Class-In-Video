383aae4c67f682755b762c4a96ebfff83a5ba85a
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2022-11-19 00:19:28 -0500

    Challenge-8

---
+++ Desafio008.py
@@ -0,0 +1,9 @@
+'''metro = float(input('Digite um valor em metros: '))
+cm = 100 * metro
+mm = 1000 * metro
+print('O valor em {} metros, equivale a {:.1f} centimetros e {:.1f} milimetros'.format(metro, cm, mm))'''
+
+meters = float(input('Enter meters value: '))
+cm = 100 * meters
+mm = 1000 * meters
+print('The value of {} meters is equivalent to {:.1f} centimeters and {:.1f} millimeters'.format(meters, cm, mm))
