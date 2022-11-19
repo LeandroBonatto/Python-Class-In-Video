403f32e196da4a0b7a587608377abf7f8572af84
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2022-11-19 00:17:39 -0500

    Challenge-5

---
+++ Desafio005.py
@@ -0,0 +1,9 @@
+# n = int(input('Escreva algum valor: '))
+# a = n - 1
+# s = n + 1
+# print('Considerando o valor {}, seu antecessor sera {:.2f} e o sucessor sera {:.2f}'.format(n, a, s))
+
+n = int(input('Enter some value: '))
+pred = n - 1
+suc = n + 1
+print('Considering the value of {}, its predecessor will be {:.2f} and the successor will be {:.2f}'.format(n, pred, suc))
