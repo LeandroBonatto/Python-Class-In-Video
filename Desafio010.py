9884866c0274a6a0934fdb4afe10547cb5bddca3
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2022-11-22 20:19:08 -0500

    Challenge-10

---
+++ Desafio010.py
@@ -0,0 +1,8 @@
+'''
+Wallet = float(input('Qnto tenho na carteira em R$: '))
+Exchange = Wallet/3.27
+print('Eu tenho na minha carteira em R$ {:.2f} e em Dolar {:.2f}'.format(Wallet, Exchange))'''
+
+Wallet = float(input('How much I have in my wallet in R$:'))
+Exchange = Wallet/3.27
+print('I have in my wallet in BRL {:.2f} and in Dollars {:.2f}'.format(Wallet, Exchange))
