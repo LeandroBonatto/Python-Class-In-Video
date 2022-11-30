f37cc4162a4b86ee57285d48fdcaf093ac5725d6
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2022-11-30 00:03:12 -0500

    Challenge-14

---
+++ Desafio014.py
@@ -0,0 +1,8 @@
+'''
+c = float(input('Informe a temperatura em graus C: '))
+# f = 9*c /5 +32
+# print('A temperatura de {}C corresponde a {:.1f}F'.format(c, f)) '''
+
+c = float(input('Enter temperature in degrees C: '))
+f = 9*c /5 +32
+print('The temperature of {}C corresponds to {:.1f}F'.format(c, f))
