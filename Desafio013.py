d1832d9231fe1ee6df2847223a3ee40be84a8cb0
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2022-11-29 22:23:01 -0500

    Delete Desafio013.py

--- Desafio013.py
+++
@@ -1,9 +0,0 @@
-# salario = float(input('Qual eh o seu salario? '))
-# aumento = salario * 15 / 100
-# total = salario + aumento
-# print('O meu salario eh R$ {}, com o aumento, meu novo salario sera R$ {:.2f}'.format(salario, total))
-
-wage = float(input('What is your wage? '))
-subsidy = wage * 15 / 100
-total = wage + subsidy
-print('My salary is U$ {}, with the increase, my new salary will be U$ {:.2f}'.format(wage, total))
