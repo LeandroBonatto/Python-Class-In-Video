8f23588539292c5e43bfba1fcf7055a55750f644
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2022-11-30 00:03:37 -0500

    Challenge-14-Ex1

---
+++ desafio014ex.py
@@ -0,0 +1,8 @@
+'''
+f = float(input('Informe a temperatura em graus F: '))
+c = (f - 32) / 1.8
+print('A temperatura de Fahrenheit {} para graus Celsius eh de {}'.format(f, c)) '''
+
+f = float(input('Enter temperature in degrees F: '))
+c = (f - 32) / 1.8
+print('The temperature from Fahrenheit {} to degrees Celsius is {}'.format(f, c))
