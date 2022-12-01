f2249bad6f145f60bec12ec91e74b0efd242555a
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2022-12-01 00:27:01 -0500

    Lesson-8-A

---
+++ Aula08-A.py
@@ -0,0 +1,9 @@
+import math
+'''num = int(input('Digite um numero: '))
+raiz = math.sqrt(num)
+print('A raiz de {} eh igual a {:.2f}'.format(num, raiz))
+'''
+
+num = int(input('Enter a number: '))
+root = math.sqrt(num)
+print('Square root of {} is equal to {:.2f}'.format(num, root))
