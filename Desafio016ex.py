696e495c3679be785637e1e9765ace719370aedc
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2022-12-01 00:30:08 -0500

    Challenge-16-Ex

---
+++ Desafio016ex.py
@@ -0,0 +1,6 @@
+import random
+'''num = random.random()
+print('Digite um numero: {:.2f}. O numero {:.2f} tem a parte inteira {}'.format(num, num, int(num)))'''
+
+num = random.random()
+print('Enter a number: {:.2f}. The number {:.2f} has the integer part {}'.format(num, num, int(num)))
