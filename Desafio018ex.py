a494e31c68e9b96e3464a495b4442fee6dd1bfd9
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2022-12-01 00:32:23 -0500

    Challenge-18-Ex

---
+++ Desafio018ex.py
@@ -0,0 +1,18 @@
+from math import radians, sin, cos, tan
+a = float(input('Set an angle: '))
+sine = sin(radians(a))
+print('The angle of {} has SINE of {:.2f}'.format(a, sine))
+cosine = cos(radians(a))
+print('The angle of {} has COSINE of {:.2f}'.format(a, cosine))
+tangent = tan(radians(a))
+print('The angle of {} has a TANGENT of {:.2f}'.format(a, tangent))
+
+
+'''a = float(input('Difite um angulo: '))
+seno = sin(radians(a))
+print('O angulo de {} tem SENO de {:.2f}'.format(a, seno))
+cosseno = cos(radians(a))
+print('O angulo de {} tem COSSENO de {:.2f}'.format(a, cosseno))
+tangente = tan(radians(a))
+print('O angulo de {} tem TANGENTE de {:.2f}'.format(a, tangente))'''
+
