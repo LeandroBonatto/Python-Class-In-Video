de36f736d3ac5a802e9ac5d4b73d1ca3e93baf15
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2022-12-01 00:30:49 -0500

    Challenge-17

---
+++ Desafio017.py
@@ -0,0 +1,23 @@
+from math import hypot
+co = float(input('Length of opposite side: '))
+ca = float(input('Length of adjacent side: '))
+hi = hypot(co, ca)
+print('The hypotenuse will measure {:.2f}'.format(hi))
+
+import math
+co = float(input('Length of opposite side: '))
+ca = float(input('Length of adjacent side: '))
+hi = math.hypot(co, ca)
+print('The hypotenuse will measure {:.2f}'.format(hi))
+
+'''from math import hypot
+co = float(input('Comprimento do cateto oposto: '))
+ca = float(input('Comprimento do cateto adjacente: '))
+hi = hypot(co, ca)
+print('A hipotenusa vai medir {:.2f}'.format(hi))
+
+import math
+co = float(input('Comprimento do cateto oposto: '))
+ca = float(input('Comprimento do cateto adjacente: '))
+hi = math.hypot(co, ca)
+print('A hipotenusa vai medir {:.2f}'.format(hi))'''
