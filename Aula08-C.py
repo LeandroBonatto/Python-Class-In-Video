6151c73491ef1c3b2f74b3273d25bdd46e2cf81f
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2022-12-01 00:27:51 -0500

    Lesson-8-C

---
+++ Aula08-C.py
@@ -0,0 +1,8 @@
+from math import sqrt, floor
+'''num = int(input('Digite um numero: '))
+raiz = sqrt(num)
+print('A raiz de {} eh igual a {:.2f}'.format(num, floor(raiz)))'''
+
+num = int(input('Enter a number: '))
+root = sqrt(num)
+print('The root of {} is equal to {:.2f}'.format(num, floor(root)))
