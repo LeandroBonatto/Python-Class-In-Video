3e3456e738bdc6382ec245734a16d67182e690d8
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2022-12-01 00:29:44 -0500

    Challenge-16

---
+++ Desafio016.py
@@ -0,0 +1,12 @@
+from math import trunc
+'''num = float(input('Digite um valor: '))
+print('O valor digitado foi {} e a sua porcao inteira eh {}'.format(num, trunc(num)))
+
+num = float(input('Digite um valor: '))
+print('O valor digitado foi {} e a sua porcao inteira eh {}'.format(num, int(num)))'''
+
+num = float(input('Enter a value: '))
+print('The value entered was {} and its entire portion is {}'.format(num, trunc(num)))
+
+num = float(input('Enter a value: '))
+print('The value entered was {} and its entire portion is {}'.format(num, int(num)))
