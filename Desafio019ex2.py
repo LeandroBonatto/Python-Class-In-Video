c8983d3e202e4686e422914e60ea19e15af0ca37
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2022-12-06 00:04:50 -0500

    Challenge-019-Ex2

---
+++ Desafio019ex2.py
@@ -0,0 +1,18 @@
+import random
+n1 = str('Numero 1')
+n2 = str('numero 2')
+n3 = str('Numero 3')
+n4 = str('numero 4')
+n5 = str('Numero 5')
+n6 = str('numero 6')
+n7 = str('Numero 7')
+n8 = str('numero 8')
+n9 = str('Numero 9')
+n10 = str('numero 10')
+list = [n1, n2, n3, n4]
+chosen = random.choice(list)
+print('selected {}'.format(chosen) )
+
+'''lista = [n1, n2, n3, n4]
+escolhido = random.choice(lista)
+print('Foi sorteado o {}'.format(escolhido))'''
