f23dd3670b13514b51a3b76cdf21bdd4678fcfaa
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2022-12-06 23:42:35 -0500

    Challenge-20

---
+++ Desafio020.py
@@ -0,0 +1,20 @@
+from random import shuffle
+n1 = str(input('First student: '))
+n2 = str(input('Second student: '))
+n3 = str(input('Third student: '))
+n4 = str(input('Fourth student: '))
+list = [n1, n2, n3, n4]
+shuffle(list)
+print('The order of presentation will be ')
+print(list)
+
+'''import random
+n1 = str(input('Primeiro aluno: '))
+n2 = str(input('Segundo aluno: '))
+n3 = str(input('Terceiro aluno: '))
+n4 = str(input('Quarto aluno: '))
+lista = [n1, n2, n3, n4]
+random.shuffle(lista)
+print('A ordem de apresentacao sera ')
+print(lista)'''
+
