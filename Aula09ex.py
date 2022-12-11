a2440845f6c2ce89fd436519d59120b785730f54
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2022-12-11 01:09:34 -0500

    Lesson-9

---
+++ Aula09ex.py
@@ -0,0 +1,53 @@
+'''frase = 'Curso em video python'
+print(frase.split())'''
+
+'''frase = 'Curso em video python'
+print = (frase[3])'''
+
+frase = 'Curso em video python'
+print(frase[3:13])
+
+'''frase = 'Curso em video python'
+print(frase[1:15:2])'''
+
+'''frase = 'Curso em video python'
+print(frase[::2])'''
+
+'''frase = 'Curso em video python'
+print(frase.count('o'))'''
+
+'''frase = 'Curso em video python'
+print(frase.count('O'))'''
+
+'''frase = 'Curso em video python'
+print(frase.upper().count('O'))'''
+
+'''frase = 'Curso em video python'
+print(len(frase))'''
+
+'''frase = 'Curso em video python'
+print(len(frase.strip()))'''
+
+'''frase = 'Curso em video python'
+print(frase.replace('python', 'android'))'''
+
+'''frase = 'Curso em video python'
+frase = frase.replace('python', 'android')
+print(frase)'''
+
+'''frase = 'Curso em video python'
+print('Curso' in frase)'''
+
+'''frase = 'Curso em video python'
+print(frase.find('Curso'))'''
+
+'''frase = 'Curso em video python'
+print(frase.lower().find('video'))'''
+
+'''frase = 'Curso em video python'
+dividido = frase.split()
+print(dividido[0])'''
+
+'''frase = 'Curso em video python'
+dividido = frase.split()
+print(dividido[0][3])'''
