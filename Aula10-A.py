825ec4f56cf5f90a32c7ac617211641443a920e4
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2023-01-19 00:07:43 -0500

    Class10-A

---
+++ Aula10-A.py
@@ -0,0 +1,9 @@
+name = str(input('What is your name? '))
+if name == 'Gustavo':
+    print('What a beautiful name you have!')
+print('Good morning, {}'.format(name))
+
+'''nome = str(input('Qual eh seu nome? '))
+if nome == 'Gustavo':
+    print('Que nome bonito vc tem!')
+print('Bom dia, {}'.format(nome))'''
