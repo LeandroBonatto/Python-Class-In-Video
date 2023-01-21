dfa7e891872a32c570b2a29710a210084083ee4c
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2023-01-21 02:01:32 -0500

    Class-10-C

---
+++ Aula10-C.py
@@ -0,0 +1,13 @@
+n1 = float(input('Enter the first note: '))
+n2 = float(input('Enter the second note: '))
+m = (n1 + n2) / 2
+print('Your average was {:.1f}'.format(m))
+if m >= 6.0:
+     print('You reached the score! CONGRATULATIONS!')
+
+'''n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
+m = (n1 + n2) / 2
+print('A sua media foi {:.1f}'.format(m))
+if m >= 6.0:
+    print('Sua media foi boa! PARABENS!')'''
