66bbd727dfa8c15af7ad1cef1fa6492a1f75e0ab
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2023-01-25 22:35:01 -0500

    Challenge-29-Ex1

---
+++ Desafio029ex.py
@@ -0,0 +1,9 @@
+speed = float(input('What is the speed of the car? '))
+if speed > 80: print('FINE! You exceeded the speed limit')
+print('Have a nice day! Drive safely!')
+
+
+'''veloc = float(input('Qual eh a velocidade do carro? '))
+if veloc > 80:
+    print('MULTADO! Vc excedeu o limite permitido que eh de 80 km/h')
+print('Tenha um bom dia! Dirija com segurança!')'''
