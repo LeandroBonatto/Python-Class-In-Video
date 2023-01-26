76ff84a4f0c427b68b14a66909c92ed8d61f8e49
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2023-01-25 22:35:53 -0500

    Challenge-30-Ex1

---
+++ Desafio030ex.py
@@ -0,0 +1,7 @@
+num = int(input('Tell me any number: '))
+res = num % 2 # remainder of division
+print('The result was {}'.format(res))
+
+'''num = int(input('Me diga um numero qualquer: '))
+res = num % 2   # resto da divisao
+print('O resultado foi {}'.format(res))'''
