a02f2e2090631f98ff89bdcf013608bb64e8db49
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2023-02-08 12:16:44 -0500

    Lesson-12-A

---
+++ Aula12-A.py
@@ -0,0 +1,17 @@
+name = str(input('What is your name? '))
+if name == 'Gustavo':
+     print('What a beautiful name!')
+elif name == 'Peter' or name == 'Maria' or name == 'Paul':
+     print('Your name is very popular in Brazil.')
+else:
+     print('Your name is pretty normal.')
+print('Have a nice day, {}!'.format(name))
+
+'''nome = str(input('Qual eh seu nome? '))
+if nome == 'Gustavo':
+    print('Que nome bonito!')
+elif nome == 'Pedro' or nome == 'Maria' or nome == 'Paulo':
+    print('Seu nome eh bem popular no Brasil.')
+else:
+    print('Seu nome eh bem normal.')
+print('Tenha um bom dia, {}!'.format(nome))'''
