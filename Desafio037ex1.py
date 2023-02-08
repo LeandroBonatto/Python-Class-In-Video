3695edbaa7915c845a61309b3ed841c09de9a1d1
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2023-02-08 12:19:38 -0500

    Challenge-37-Ex1

---
+++ Desafio037ex1.py
@@ -0,0 +1,6 @@
+num = int(input('Digite um numero inteiro: '))
+print('''Escolha uma das bases para conversao:
+ [ 1 ] converter para BINARIO
+ [ 2 ] converter para OCTAL
+ [ 3 ] converter para HEXADECIMAL''')
+opcao = int(input('Sua opcao: '))
