1f360972da2c7f48ceec320f294ff161a6a10dc1
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2023-02-28 17:04:11 -0500

    Lesson-13-F

---
+++ Aula13-F.py
@@ -0,0 +1,14 @@
+start = int(input('Start: '))
+e = int(input('End: '))
+step = int(input('Step: '))
+for c in range(start, e+1, step):
+     print(c)
+print('END')
+
+
+'''i = int(input('Inicio: '))
+f = int(input('Fim: '))
+p = int(input('Passo: '))
+for c in range(i, f+1, p):
+    print(c)
+print('FIM')'''
