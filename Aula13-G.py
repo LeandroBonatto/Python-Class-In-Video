164399f6e6d669d545b5fac21a9092d4d7a57ff7
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2023-02-28 17:04:44 -0500

    Lesson-13-G

---
+++ Aula13-G.py
@@ -0,0 +1,12 @@
+s = 0
+for c in range(0, 4):
+     n = int(input('Enter a value: '))
+     s += n # s = y + n
+print('The sum of all values was {}'.format(s))
+
+
+'''s = 0
+for c in range (0, 4):
+    n = int(input('Digite um valor: '))
+    s += n       # s = s + n
+print('O somatorio de todos os valores foi {}'.format(s))'''
