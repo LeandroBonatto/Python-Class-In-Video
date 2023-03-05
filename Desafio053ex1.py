7a64ce71bf401e8515b5368c71702fd8f7f6e41c
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2023-03-05 16:16:32 -0500

    Challenge-53-Ex1

---
+++ Desafio053ex1.py
@@ -0,0 +1,10 @@
+phrase = str(input('Enter a phrase: ')).strip().upper()
+words = phrase.split()
+together = '#'.join(words)
+print('You typed the sentence {}'.format(together))
+
+
+'''frase = str(input('Digite uma frase: ')).strip().upper()
+palavras = frase.split()
+junto = '#'.join(palavras)
+print('Vc digitou a frase {}'.format(junto))'''
