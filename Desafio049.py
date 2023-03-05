a1f3e7770a1f7b79e0adc4fd12d3b31d9fd4fd2e
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2023-03-05 16:15:06 -0500

    Challenge-49

---
+++ Desafio049.py
@@ -0,0 +1,8 @@
+num = int(input('Enter a number to see the multiplication table: '))
+for t in range(1, 11):
+     print('{} x {:2} = {}'.format(num, t, num*t))
+
+
+'''num = int(input('Digite um numero para ver a tabuada: '))
+for t in range(1, 11):
+    print('{} x {:2} = {}'.format(num, t, num*t))'''
