2ff0ef91e8d440c3691e5b54d73d6684f01b005d
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2023-03-05 16:16:15 -0500

    Challenge-53

---
+++ Desafio053.py
@@ -0,0 +1,24 @@
+phrase = str(input('Enter a phrase: ')).strip().upper()
+words = phrase.split()
+together = ''.join(words)
+inverse = ''
+for letter in range (len(together) -1, -1, -1):
+     inverse += together[letter]
+print('The inverse of {} is {}'.format(together, inverse))
+if inverse == together:
+     print('We have a palindrome!')
+else:
+     print('No palindrome!')
+
+
+'''frase = str(input('Digite uma frase: ')).strip().upper()
+palavras = frase.split()
+junto = ''.join(palavras)
+inverso = ''
+for letra in range (len(junto) -1, -1, -1):
+    inverso += junto[letra]
+print('O inverso de {} eh {}'.format(junto, inverso))
+if inverso == junto:
+    print('Temos um palindromo!')
+else:
+    print('Nao eh palindromo!')'''
