5886f1a3a3a461fdeab172695417ed1f06dab770
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2023-03-06 00:13:11 -0500

    Lesson-14-C

---
+++ Aula14-C.py
@@ -0,0 +1,12 @@
+r = 'S'
+while r == 'S':
+     n = int(input('Enter a value: '))
+     r = str(input('Do you want to continue? [Y/N] ')).upper()
+print('End')
+
+
+'''r = 'S'
+while r == 'S':
+    n = int(input('Digite um valor: '))
+    r = str(input('Quer continuar? [S/N] ')).upper()
+print('Fim')'''
