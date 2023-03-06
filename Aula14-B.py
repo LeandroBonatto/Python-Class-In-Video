e8a0e7ab4c0367de70ddf6d6d5d3a9905cc07b19
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2023-03-06 00:12:26 -0500

    Lesson-14-B

---
+++ Aula14-B.py
@@ -0,0 +1,10 @@
+# CONDICAO DE PARADA
+n = 1
+while n != 0:
+     n = int(input('Enter a value: '))
+print('End')
+
+'''n = 1
+while n != 0:
+    n = int(input('Digite um valor: '))
+print('Fim')'''
