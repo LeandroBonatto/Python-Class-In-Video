a12a4149c598bbd90b8dd9304330aa43c7e2fa96
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2023-03-08 19:16:26 -0500

    Add files via upload

---
+++ Aula#15/Aula15-C.py
@@ -0,0 +1,7 @@
+n = 0
+while n != 999: #flag - Only stops when typing 999
+     n = int(input('Enter a number: '))
+
+'''n = 0
+while n != 999: #flag - So para quando digitar 999
+    n = int(input('Digite um numero: '))'''
