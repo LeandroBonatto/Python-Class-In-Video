b1609b0e54c02b5e4e055605499c89d22186a861
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2023-03-11 11:02:05 -0500

    Lesson-15-A

---
+++ Aula15-A.py
@@ -0,0 +1,11 @@
+count = 1
+# while True: # infinite looping, press stop or break to stop!
+     print(count, '...', end='')
+     count += 1
+print('It is over')
+
+'''cont = 1
+# while True: # looping infito, apertar stop ou break pra parar!
+    print(cont, '...', end='')
+    cont += 1
+print('Acabou')'''
