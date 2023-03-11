cd135602b6a8c061e4ee826ae1ddcbe15335aa93
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2023-03-11 11:03:18 -0500

    Lesson-15-B

---
+++ Aula15-B.py
@@ -0,0 +1,9 @@
+n = count = 0
+while count < 3: #counter reading 3 numbers
+     n = int(input('Enter a number: '))
+     count += 1
+
+'''n = cont = 0
+while cont < 3: #contador lendo 3 numeros
+    n = int(input('Digite um numero: '))
+    cont += 1'''
