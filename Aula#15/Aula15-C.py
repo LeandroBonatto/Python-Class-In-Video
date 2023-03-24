65ba510c1f892653fbf4c6cc439b138ad727c266
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2023-03-23 23:01:39 -0400

    Delete Aula#15 directory

--- Aula#15/Aula15-C.py
+++
@@ -1,7 +0,0 @@
-n = 0
-while n != 999: #flag - Only stops when typing 999
-     n = int(input('Enter a number: '))
-
-'''n = 0
-while n != 999: #flag - So para quando digitar 999
-    n = int(input('Digite um numero: '))'''
