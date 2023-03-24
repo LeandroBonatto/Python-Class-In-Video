d463f7a3a0102ee1a332c9eaa516fbf96a110088
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2023-03-23 23:04:33 -0400

    Delete Aula#12 directory

--- Aula#12/Desafio037ex1.py
+++
@@ -1,6 +0,0 @@
-num = int(input('Digite um numero inteiro: '))
-print('''Escolha uma das bases para conversao:
- [ 1 ] converter para BINARIO
- [ 2 ] converter para OCTAL
- [ 3 ] converter para HEXADECIMAL''')
-opcao = int(input('Sua opcao: '))
