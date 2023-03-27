09c71da2bb16ddb1dacac7e73e036ca0c4964bc1
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2023-03-26 22:41:38 -0400

    Lesson-16-B

---
+++ Aula16-B.py
@@ -0,0 +1,18 @@
+snack = ('hamburger', 'juice', 'Pizza', 'Pudding')
+print(snack[1])
+
+snack = ('hamburger', 'juice', 'Pizza', 'Pudding')
+print(snack[-2])
+
+snack = ('hamburger', 'juice', 'Pizza', 'Pudding')
+print(snack[1:3])
+
+
+'''lanche = ('hamburguer', 'suco', 'Pizza', 'Pudim')
+print(lanche[1])
+
+lanche = ('hamburguer', 'suco', 'Pizza', 'Pudim')
+print(lanche[-2])
+
+lanche = ('hamburguer', 'suco', 'Pizza', 'Pudim')
+print(lanche[1:3])'''
