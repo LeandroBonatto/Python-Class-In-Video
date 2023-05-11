62be6c609ec572913c7dee83dd675f78d51a523b
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2023-05-11 15:00:55 -0400

    Lesson-17-D

---
+++ Aula17-D.py
@@ -0,0 +1,24 @@
+num = [2, 5, 9, 1]
+num[2] = 3
+num.append(7)
+num.sort(reverse=True)
+num.insert(2, 2)
+if 4 in num:
+     num.remove(4)
+else:
+     print('I couldnt find the number 4')
+print(num)
+print(f'This list has {len(num)} elements.')
+
+
+'''num = [2, 5, 9, 1]
+num[2] = 3
+num.append(7)
+num.sort(reverse=True)
+num.insert(2, 2)
+if 4 in num:
+    num.remove(4)
+else:
+    print('Nao achei o numero 4')
+print(num)
+print(f'Essa lista tem {len(num)} elementos.')'''
