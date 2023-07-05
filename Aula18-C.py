398b4be4f6d9f85a49b99322a42129c56a91db14
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2023-07-05 11:32:57 -0400

    Lesson-18-C

---
+++ Aula18-C.py
@@ -0,0 +1,11 @@
+galera = [['Joao', 19], ['Ana', 33], ['Joaquim', 13], ['Maria', 45]]
+for p in galera:
+    print(p)
+print('=-' * 15)
+galera = [['Joao', 19], ['Ana', 33], ['Joaquim', 13], ['Maria', 45]]
+for p in galera:
+    print(p[0])
+print('=-' * 15)
+galera = [['Joao', 19], ['Ana', 33], ['Joaquim', 13], ['Maria', 45]]
+for p in galera:
+    print(p[1])
