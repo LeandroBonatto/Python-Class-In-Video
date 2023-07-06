8eee497bf9d2c1cd99929613f4515280ac732ece
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2023-07-06 11:22:03 -0400

    Lesson-19-I

---
+++ Aula19-I.py
@@ -0,0 +1,12 @@
+estado = dict()
+brasil = list()
+for c in range(0, 3):
+    estado['uf'] = str(input('Unidade Federativa: '))
+    estado['sigla'] = str(input('Sigla do estado: '))
+    brasil.append(estado.copy())  # usar o [:] para copiar os fatiamentos dos dados da errado. Usar copy.
+for e in brasil:
+    for v in e.values():
+        print(v)
+
+        #print(v, end=' ')
+        #print()
