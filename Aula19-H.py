309b79abcab8bdd2bc1fcd4cd9c14b44f7539f62
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2023-07-06 11:21:19 -0400

    Lesson-19-H

---
+++ Aula19-H.py
@@ -0,0 +1,9 @@
+estado = dict()
+brasil = list()
+for c in range(0, 3):
+    estado['uf'] = str(input('Unidade Federativa: '))
+    estado['sigla'] = str(input('Sigla do estado: '))
+    brasil.append(estado.copy())  # usar o [:] para copiar os fatiamentos dos dados da errado. Usar copy.
+for e in brasil:
+    for k, v in e.items():
+        print(f'O campo {k} tem valor {v}.')
