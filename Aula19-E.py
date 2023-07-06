e58c7c36c5a0270c200c8b71841919822ee3ec57
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2023-07-06 11:17:17 -0400

    Lesson-19-E

---
+++ Aula19-E.py
@@ -0,0 +1,15 @@
+brasil = []
+estado1 = {'uf': 'Rio de Janeiro', 'sigla': 'RJ'}
+estado2 = {'uf': 'Sao paulo', 'sigla': 'SP'}
+brasil.append(estado1)
+brasil.append(estado2)
+
+print(brasil[0])
+print('=' * 40)
+print(brasil[1])
+print('=' * 40)
+print(brasil[0]['uf'])
+print('=' * 40)
+print(brasil[0]['sigla'])
+print('=' * 40)
+print(brasil[1]['sigla'])
