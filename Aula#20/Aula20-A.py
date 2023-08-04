2fad954c12b27906025557ed0d653d01e2b41eec
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2023-08-04 11:17:15 -0400

    Class#20

---
+++ Aula#20/Aula20-A.py
@@ -0,0 +1,18 @@
+def lin():
+    print('-' * 30)
+
+
+# Programa Principal
+lin()
+print('    CURSO EM VIDEO    ')
+print('    APRENDA PYTHON    ')
+print('    GUSTAVO GUANABARA ')
+lin()
+
+lin()
+print('    CURSO EM VIDEO    ')
+lin()
+print('    APRENDA PYTHON    ')
+lin()
+print('    GUSTAVO GUANABARA ')
+lin()
