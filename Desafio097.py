7296fc7221fb49a2b588f0283dd07af2b0f4b3cf
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2023-08-06 09:02:18 -0400

    Challenge-97

---
+++ Desafio097.py
@@ -0,0 +1,10 @@
+def escreva(msg):
+    tam = len(msg) + 4
+    print('~' * tam)
+    print(f'  {msg}')
+    print('~' * tam)
+
+
+escreva('Curso em Video')
+escreva('Curso de Python no Youtube')
+escreva('Cev')
